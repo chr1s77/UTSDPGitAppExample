41f79b32129d9173eca79510b3548129d497d355
Christine Julien <christinejulien@Christines-MBP.attlocal.net>
2017-06-13 09:01:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.utsdpgitappexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+    // Array of strings...
+    String[] classNames = {"Christine Julien"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, classNames);
+
+        ListView listView = (ListView) findViewById(R.id.roster);
+        listView.setAdapter(adapter);
+    }
+}
