e7a8d189d60dfe3b13c0d72ba3923840b0987e7b
yosefsaputra <yosef.saputra@gmail.com>
2018-06-30 10:56:30 -0500

    To add Yosef to class roster

--- app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
+++ app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
     // Array of strings...
-    String[] classNames = {"Christine Julien"};
+    String[] classNames = {"Christine Julien", "Yosef"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
