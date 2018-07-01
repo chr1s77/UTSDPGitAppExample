d3757a72f90a39bd04399780f7ea6cd2fd230894
= <=>
2018-07-01 16:51:09 -0500

    Added Aidan Dougherty to roster

--- app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
+++ app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
     // Array of strings...
-    String[] classNames = {"Christine Julien"};
+    String[] classNames = {"Christine Julien", "Aidan Dougherty"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
