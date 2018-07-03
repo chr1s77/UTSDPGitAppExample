00bd6c9b9c04645380d9f7cb9a58fb09835f7506
JBYoshi <jbyoshi@users.noreply.github.com>
2018-07-02 23:17:14 -0500

    Add my name.

--- app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
+++ app/src/main/java/com/example/utsdpgitappexample/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
     // Array of strings...
-    String[] classNames = {"Christine Julien", "Lilly Tian"};
+    String[] classNames = {"Christine Julien", "Lilly Tian", "Jonathan Browne"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
